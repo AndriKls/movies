2a7ab27629112e496468f967ad2622447e080bf9
AndriKls <158137294+AndriKls@users.noreply.github.com>
2024-11-24 17:07:13 +0200

    Implemented movies API

--- movies/settings.py
+++ movies/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'films',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_URL = '/media/'
--- movies/urls.py
+++ movies/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from films.views import MovieViewSet, ActionViewSet, DramaViewSet
+from django.conf.urls.static import static
+from django.conf import settings
+
+router = routers.SimpleRouter()
+router.register('movies', MovieViewSet, basename='movies')
+router.register('action', ActionViewSet, basename='action')
+router.register('drama', DramaViewSet, basename='drama')
 
 urlpatterns = [
+    path('', include(router.urls)),  # Include the router URLs)
     path('admin/', admin.site.urls),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Include media URLs for serving static files
